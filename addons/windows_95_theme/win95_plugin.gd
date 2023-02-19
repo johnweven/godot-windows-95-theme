95edf90353f37b59635bb7242c9c6b4384498603
johnweven <john.weven@gmail.com>
2023-02-18 19:34:38 -0800

    Fixed missing win95_plugin.gd bug

--- addons/windows_95_theme/win95_plugin.gd
+++ addons/windows_95_theme/win95_plugin.gd
@@ -3,8 +3,13 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	var interface: EditorInterface = get_editor_interface()
+	var settings: EditorSettings = interface.get_editor_settings()
+	settings.set("interface/theme/preset", "Custom")
+	settings.set("interface/theme/base_color", Color.SILVER)
+	settings.set("interface/theme/contrast", -0.15)
+	var theme_path = ProjectSettings.globalize_path("res://addons/windows_95_theme/save1.tres")
+	settings.set("interface/theme/custom_theme", theme_path)
 
 
 func _exit_tree():
